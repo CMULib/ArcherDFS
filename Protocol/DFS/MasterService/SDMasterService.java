d0bfad4464ee53e603a5c5be963490a52ce8e826
Rui Wang <amaliujia@users.noreply.github.com>
2015-03-26 14:05:50 -0400

    Update SDMasterService.java

--- Protocol/DFS/MasterService/SDMasterService.java
+++ Protocol/DFS/MasterService/SDMasterService.java
@@ -12,9 +12,6 @@ public interface SDMasterService extends Remote {
 
     public void heartbeat(String serviceName, String registryHost, int registryPort, int chunkNumber, boolean logable)
                                         throws RemoteException;
-    //create or update nodes, actually it is a kind of heartbeats
-    public void updateDataNode(String serviceName, String registryHost, int registryPort,
-                               int chunkNumber, long timestamp, boolean logable) throws RemoteException;
 
     //delete a node
     public void deleteDataNode(String serviceName, boolean logable) throws RemoteException;
